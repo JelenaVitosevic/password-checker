3f210dbf43a61700b7f7a9c938aeefc7f2a3c536
JelenaVitosevic <vitosevicjelena83997@gmail.com>
2022-04-13 13:12:59 +0200

    applied some changes in css and js file

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 function passwordChecker () {
-    const unetaVrednost = document.getElementById('input').value;
+    const password = document.getElementById('input').value;
     const upperCases = /[A-Z]/;
     const lowerCases = /[a-z]/;
     const nums = /[0-9]/;
-    const car = /[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/;
     let sumUpperCases = 0;
     let sumLowerCases = 0;
     let sumNums = 0;
     let sumCar = 0;
-    for (i=0; i < unetaVrednost.length; i++) {
+    for (i=0; i < password.length; i++) {
         
-        if (upperCases.test(unetaVrednost[i])) {
+        if (upperCases.test(password[i])) {
             sumUpperCases = sumUpperCases + 1;
             document.getElementById('upp').textContent = sumUpperCases;
         }
-        else if (lowerCases.test(unetaVrednost[i])) {
+        else if (lowerCases.test(password[i])) {
             sumLowerCases = sumLowerCases +1;
             document.getElementById('lower').textContent = sumLowerCases;
         }
-        else if (nums.test(unetaVrednost[i])) {
+        else if (nums.test(password[i])) {
             sumNums = sumNums +1;
             document.getElementById('num').textContent = sumNums;
         }
-        else if (car.test(unetaVrednost[i])) {
+        else {
             sumCar = sumCar +1;
             document.getElementById('car').textContent = sumCar;
         }
