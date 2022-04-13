9231f8f0076ff8f71c840f2ff62a87ca3987e9b2
JelenaVitosevic <vitosevicjelena83997@gmail.com>
2022-04-13 15:02:16 +0200

    fixing the passwordChecker function and styled page just a little bit more

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-checker
+
+This is my first task in MQ Soft. 
--- main.js
+++ main.js
@@ -8,24 +8,23 @@ function passwordChecker () {
     let sumNums = 0;
     let sumCar = 0;
     for (i=0; i < password.length; i++) {
-        
         if (upperCases.test(password[i])) {
             sumUpperCases = sumUpperCases + 1;
-            document.getElementById('upp').textContent = sumUpperCases;
         }
         else if (lowerCases.test(password[i])) {
             sumLowerCases = sumLowerCases +1;
-            document.getElementById('lower').textContent = sumLowerCases;
         }
         else if (nums.test(password[i])) {
             sumNums = sumNums +1;
-            document.getElementById('num').textContent = sumNums;
         }
         else {
             sumCar = sumCar +1;
-            document.getElementById('car').textContent = sumCar;
         }
     }
+    document.getElementById('upp').textContent = sumUpperCases;
+    document.getElementById('lower').textContent = sumLowerCases;
+    document.getElementById('num').textContent = sumNums;
+    document.getElementById('car').textContent = sumCar;
 }
 
 const mojButton = document.getElementById('button');
