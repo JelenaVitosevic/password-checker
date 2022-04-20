10d4d346672ef4140ee1715be111dce2107f91d3
JelenaVitosevic <vitosevicjelena83997@gmail.com>
2022-04-20 13:26:46 +0200

    Password checker is moved in server.js - backend (express.js), and we used fetch request in main.js to forward password input value to backend, run it through the function, and return it to the front

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-function passwordChecker () {
-    const password = document.getElementById('input').value;
-    const upperCases = /[A-Z]/;
-    const lowerCases = /[a-z]/;
-    const nums = /[0-9]/;
-    let sumUpperCases = 0;
-    let sumLowerCases = 0;
-    let sumNums = 0;
-    let sumCar = 0;
-    for (i=0; i < password.length; i++) {
-        if (upperCases.test(password[i])) {
-            sumUpperCases++;
-        }
-        else if (lowerCases.test(password[i])) {
-            sumLowerCases++;
-        }
-        else if (nums.test(password[i])) {
-            sumNums++;
-        }
-        else {
-            sumCar++;
-        }
-    }
-    const col = document.getElementsByClassName('col');
-    col[0].textContent = sumUpperCases;
-    col[1].textContent = sumLowerCases;
-    col[2].textContent = sumNums;
-    col[3].textContent = sumCar;
-}
-
 const mojButton = document.getElementById('button');
-mojButton.addEventListener('click', passwordChecker);
+
+const fetchuj = async () => {
+    const inputValue = document.getElementById('input').value;
+    const dataToSend = {
+        password: inputValue
+    }
+    const checkPassword = await fetch('http://localhost:3000/passwordChecker', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataToSend)
+    });
+    const content = await checkPassword.json();
+  
+    const col = document.getElementsByClassName('col');
+    col[0].textContent = content.upp;
+    col[1].textContent = content.low;
+    col[2].textContent = content.num;
+    col[3].textContent = content.car;
+  }
+
+mojButton.addEventListener('click', fetchuj);
 
 
 
